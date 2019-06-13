cad5529a2004a43aed4986c017c0b00f17ff8866
Lorenzo Andrees <lordandrees@gmail.com>
2019-06-12 22:36:38 -0400

    Archivos de la app añadidos

---
+++ App/src/appinventor/ai_matriaxx85/easy/Faq1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"184","Source":"Form","Properties":{"$Name":"Faq1","$Type":"Form","$Version":"24","AppName":"Easy","BackgroundImage":"fondo4.jpg","Sizing":"Responsive","Title":"Faq1","Uuid":"0","$Components":[{"$Name":"Disposici\u00f3nHorizontal1","$Type":"HorizontalArrangement","$Version":"3","AlignHorizontal":"2","Height":"80","Width":"330","Uuid":"-1094368474","$Components":[{"$Name":"Bot\u00f3n1","$Type":"Button","$Version":"6","Height":"50","Width":"50","Image":"AREGRESO.png","Uuid":"-828206019"}]}]}}
+|#
---
+++ App/src/appinventor/ai_matriaxx85/easy/Faq2.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"184","Source":"Form","Properties":{"$Name":"Faq2","$Type":"Form","$Version":"24","AppName":"easy","BackgroundImage":"fondo4.jpg","Sizing":"Responsive","Title":"Faq2","Uuid":"0","$Components":[{"$Name":"Disposici\u00f3nHorizontal1","$Type":"HorizontalArrangement","$Version":"3","AlignHorizontal":"2","Height":"80","Width":"330","Uuid":"432074266","$Components":[{"$Name":"Bot\u00f3n1","$Type":"Button","$Version":"6","Height":"50","Width":"50","Image":"AREGRESO.png","Uuid":"-1732029046"}]}]}}
+|#
